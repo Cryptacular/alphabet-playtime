965b9ad49417f7836acfd4e15798d1d3b0aa0391
Nick Mertens <nick.mertens@xero.com>
2022-07-26 17:28:27 +1200

    Hide nav bar on Android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useKeepAwake } from "expo-keep-awake";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AlphabetPlaytime } from "./components/AlphabetPlaytime";
+import { setVisibilityAsync } from "expo-navigation-bar";
 
 export default function App() {
   useKeepAwake();
+
+  useEffect(() => {
+    const f = async () => {
+      try {
+        await setVisibilityAsync("hidden");
+      } catch {}
+    };
+    f();
+  }, [setVisibilityAsync]);
 
   return (
     <SafeAreaProvider>
